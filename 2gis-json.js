627e40918035d43af596d9847fa94e1d9a4b4810
lgg <fed777os@gmail.com>
2020-04-20 08:01:11 +0300

    refactor parsing 2gis from puppeter(web html dom) to axios json

---
+++ 2gis-json.js
@@ -0,0 +1,5 @@
+module.exports = {
+    global: 'https://covid.2gis.ru/covid19-global.json',
+    cities: 'https://covid.2gis.ru/covid19-ru-by-territory.json',
+    russia: 'https://covid.2gis.ru/covid19-ru-history.json'
+};
--- index.js
+++ index.js
@@ -1,44 +1,19 @@
-const puppeteer = require('puppeteer');
-const monthRu2Num = require('./monthRu2Num.js');
-const selectors = require('./2gis-html.js');
+const json = require('./2gis-json');
+const axios = require('axios');
 const express = require('express');
 const app = express();
 
-const url = 'https://covid.2gis.ru/stat';
 const port = 22019;
 const host = '127.0.0.1';
-const browserPagesLimit = 5; // performance ad-hoc
+const url = 'https://covid.2gis.ru/stat';
 
 (async () => {
-    const browser = await puppeteer.launch({args: ['--no-sandbox']});
-
     app.get('/', async function (req, res) {
-        let currentPagesAmount = await browser.pages();
-        let page = null,
-            currentResult = {
-                error: '502',
-                description: 'Maximum connections opened, try again later'
-            };
-
-        if (currentPagesAmount.length < browserPagesLimit) {
-            page = await browser.newPage();
-
-            currentResult = await parse2gis(page);
-        }
+        let currentResult = await parse2gis();
 
         console.log('Send result:', currentResult);
 
         res.send(currentResult);
-
-        // clear page memory
-        if (page) {
-            await page.goto('about:blank');
-            await page.close();
-            page = null;
-            currentPagesAmount = null;
-        }
-
-        // await browser.close();
     });
 
     app.listen(port, host);
@@ -46,16 +21,16 @@ const browserPagesLimit = 5; // performance ad-hoc
     console.log('Start serving app on: localhost:' + port);
 })();
 
-async function parse2gis(page) {
-    await page.goto(url);
-
+async function parse2gis() {
     let result = {};
+
     try {
-        result.date = await getDate(page);
-        result.russia = await getRussia(page);
-        result.moscow = await getMoscow(page);
+        let russiaHistory = await getJson(json.russia);
+        result.date = getDate(russiaHistory[russiaHistory.length - 1]['date']);
+        result.russia = russiaHistory[russiaHistory.length - 1]['cases'];
+        result.moscow = await getMoscow();
     } catch (e) {
-        result.description = 'ERROR: markup changed, couldn\'t find elements';
+        result.description = 'ERROR: ' + e.toString();
     }
 
     result.description = 'This is actual parsed data about COVID-2019 in Russia/Moscow from ' + url;
@@ -67,29 +42,27 @@ async function parse2gis(page) {
 
 //###################################################################################33
 
-async function getDate(page) {
-    let currentDate = await getTextContent(page, selectors.date);
-    currentDate = currentDate.split(',')[1].trim().split(' ');
-
-    currentDate = currentDate[0] + '.' + monthRu2Num(currentDate[1]) + '.' + currentDate[2];
-
-    return currentDate;
+async function getMoscow() {
+    let data = await getJson(json.cities);
+    return data.items[0]['confirmed']
 }
 
-async function getRussia(page) {
-    return (await getTextContent(page, selectors.russia)).replace(',', '');
+function getDate(date) {
+    // input format: 2020-04-19
+    // output format: 19.4.2020
+    let currentDate = date.split('-');
+    return currentDate[2] + '.' + currentDate[1] + '.' + currentDate[0]
 }
 
-async function getMoscow(page) {
-    return (await getTextContent(page, selectors.moscow)).replace(',', '');
-}
-
-async function getTextContent(page, selector) {
-    return page.evaluate(
-        resultsSelector => {
-            let anchor = document.querySelectorAll(resultsSelector)[0];
-            return anchor.textContent.trim();
-        },
-        selector
-    );
+async function getJson(url) {
+    try {
+        const response = await axios.get(url);
+        return response.data
+    } catch (error) {
+        console.error(error);
+        return {
+            error: true,
+            description: error.toString()
+        }
+    }
 }
